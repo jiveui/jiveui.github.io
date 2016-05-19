31ef1aa462f8036de00269a6d267142d1c1716f0
Alexei Popov <alexei.popov87@gmail.com>
2016-05-19 18:30:14 +0700

    First pages commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Copy API...
+cp -r ../jive/docs/api/* ../jive-site/api/
+
+echo Build site...
+jekyll build
+
+echo Copy site...
+cp -r _site/* ../jiveui.github.io
+
+echo Send to gitnub...
+cd ../jiveui.github.io && git add . && git commit -m "Update $(date +"%F %T")" -a && git push
