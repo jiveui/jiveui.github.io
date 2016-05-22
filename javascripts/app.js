978c68474cdc1f38fc01c6ba9d6acb90f4b6a395
Alexei Popov <alexei.popov87@gmail.com>
2016-05-22 10:49:05 +0700

    added mute/unmute control

--- javascripts/app.js
+++ javascripts/app.js
@@ -184,13 +184,35 @@ jQuery(document).foundation();
 
     // stop video
     $("a[class^='sequence-'").click(function () {
-      console.log("clicked");
       $("li.animate-in").find("video").each(function() {
         this.pause();
       });
       $("li.animate-out").find("video").each(function() {
         this.play();
       });
+    });
+
+    // Video controls
+    $("#video-controls").css("position", "absolute");
+    var videocontainer = $("#video-controls").parent();
+    var top = videocontainer.offset().top + 30;
+    var left = videocontainer.width() - $("#video-controls").width() - 70;
+    $("#video-controls").offset({ top: top, left: left });
+
+    // init default icon
+    if ($(".videoslide").prop("muted"))
+      $("#mute").removeClass("fa-volume-up").addClass("fa-volume-off");
+    else
+      $("#mute").removeClass("fa-volume-off").addClass("fa-volume-up");
+
+    $("#mute").click(function() {
+      if ($(".videoslide").prop("muted")) {
+        $("#mute").removeClass("fa-volume-off").addClass("fa-volume-up");
+        $(".videoslide").prop("muted", false);
+      } else {
+        $("#mute").removeClass("fa-volume-up").addClass("fa-volume-off");
+        $(".videoslide").prop("muted", true);
+      }
     });
 
   });
